42938be7bd5e53539181a90bef6fca42b1b058ee
PAULA RIBEIRO <paulasalinor@gmail.com>
2021-12-17 14:11:29 -0300

    requisito 6 ok

--- script.js
+++ script.js
@@ -1,9 +1,64 @@
-let r = Math.floor(Math.random()*255);
-let g = Math.floor(Math.random()*255);
-let b = Math.floor(Math.random()*255);
-
 let body = document.querySelector('body');
+
+function randomColor(){
+    let color;
+    let r = Math.floor(Math.random()*255);
+    let g = Math.floor(Math.random()*255);
+    let b = Math.floor(Math.random()*255);
+
+    color = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    return color;
+}
+
 let paragraph = document.createElement('p');
 paragraph.id='rgb-color';
-paragraph.innerText = '(' + r + ', ' + g + ', ' + b + ')';
+paragraph.innerText = randomColor();
 body.appendChild(paragraph);
+
+let container = document.createElement('div');
+container.id = 'container';
+body.appendChild(container);
+
+function createdBall(){
+    let ball = document.createElement('div');
+    ball.className = 'ball';
+    ball.style.backgroundColor = randomColor();
+    container.appendChild(ball);
+}
+
+for(let index = 0; index < 6; index += 1){
+    createdBall();
+}
+
+let answer = document.createElement('p');
+answer.id = 'answer';
+answer.innerText = 'Escolha uma cor';
+body.appendChild(answer);
+
+container.addEventListener('click', compare);
+
+function compare(event){
+    if(event.target.backgroundColor === paragraph.innerText){
+        answer.innerText = 'Acertou!'
+    } else{
+        answer.innerText = 'Errou! Tente novamente!'
+    }
+}
+
+let reset = document.createElement('button');
+reset.id = 'reset-game';
+reset.innerText = 'Reiniciar';
+body.appendChild(reset);
+
+reset.addEventListener('click', reload);
+
+function reload(){
+    let balls = document.querySelectorAll('.ball');
+    for(let index = 0; index < balls.length; index += 1){
+        balls[index].style.backgroundColor = randomColor();
+    } 
+    if(answer.innerText !== 'Escolha uma cor'){
+        answer.innerText = 'Escolha uma cor'
+    }
+    paragraph.innerText = randomColor();
+} 
